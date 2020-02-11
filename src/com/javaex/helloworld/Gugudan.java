5a7b35168c79a4c54e37fffad915c7aa71d5318c
Bit <Bit@DESKTOP-16872RH>
2020-02-11 17:17:19 +0900

    day02...

--- src/com/javaex/helloworld/Gugudan.java
+++ src/com/javaex/helloworld/Gugudan.java
@@ -23,9 +23,17 @@ public class Gugudan {
 //		}
 		
 		// Gugudan 2~9
-		for(i = 2; i < 10; i++) {
-			for(int j = 1; j < 10; j++) {
-				System.out.println(i + " * " + j + " = " + i * j);
+//		for(i = 2; i < 10; i++) {
+//			for(int j = 1; j < 10; j++) {
+//				System.out.println(i + " * " + j + " = " + i * j);
+//			}
+//			System.out.println();
+//		}
+		
+		// 2~9 가로로 출력
+		for(i = 1; i < 10; i++) {
+			for(int dan = 2; dan < 10; dan++) {
+				System.out.print(dan + " * " + i + " = " + dan*i + "\t");
 			}
 			System.out.println();
 		}
--- src/com/javaex/helloworld/MultiMinNumber.java
+++ src/com/javaex/helloworld/MultiMinNumber.java
@@ -3,11 +3,14 @@ package com.javaex.helloworld;
 public class MultiMinNumber {
 	public static void main(String[] args) {
 		int i = 1;
+		int count = 0;
 
 		while (true) {
 			if ((i % 6 == 0) && (i % 14 == 0)) {
-				System.out.println(i);
-				break;
+				count++;
+				System.out.println(count + ": " + i);
+				if(count == 10)
+					break;
 			}
 			i++;
 		}
